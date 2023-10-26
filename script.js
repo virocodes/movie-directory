09b4d39bb56a5cfcf1f6eb835c4dbea44acef41a
Dharshan Bellan <dbellan1291@gmail.com>
2023-10-26 03:43:05 -0700

    Added functionality, need to fix bugs for search bar and the div classes in html

--- script.js
+++ script.js
@@ -5,3 +5,49 @@ const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?&api_key=610fa0b6cb
 const main = document.querySelector("main");
 const form = document.querySelector(".searchform");
 const search = document.querySelector(".searchbar");
+
+returnMovies(APILINK)
+    function returnMovies(url) {
+        fetch(url).then(res => res.json())
+        .then(function(data) {
+            console.log(data.results);
+            data.results.forEach(element => {
+                const image = document.createElement('img');
+                image.setAttribute('class', 'thumbnail');
+
+                const title = document.createElement('h3');
+                title.setAttribute('class', 'title');
+                
+                const div_card = document.createElement('div');
+                div_card.setAttribute('class', 'card');
+
+                const div_column = document.createElement('div');
+                div_column.setAttribute('class', 'column');
+
+                const div_row = document.createElement('div');
+                div_card.setAttribute('class', 'card');
+
+                title.innerHTML = `${element.title}`;
+                image.src = IMG_PATH + element.poster_path;
+
+                div_card.appendChild(image);
+                div_card.appendChild(title);
+                div_column.appendChild(div_card);
+                div_row.appendChild(div_column);
+
+                main.appendChild(div_row);
+            });
+        })
+    }
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    main.innerHTML = '';
+    
+    const searchItem = search.ariaValueMax;
+
+    if (searchItem) {
+        returnMovies(SEARCHAPI + searchItem);
+        search.value = "";
+    }
+})
