59b92cad48381edab6aaa3c6d11c5cd46a779a5c
Dharshan Bellan <dbellan1291@gmail.com>
2023-10-26 03:08:38 -0700

    Created basic layout and styling, got movie API link and started script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=610fa0b6cbd85c4750e4a082db62c55b&page=1';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?&api_key=610fa0b6cbd85c4750e4a082db62c55b&query=';
+
+const main = document.querySelector("main");
+const form = document.querySelector(".searchform");
+const search = document.querySelector(".searchbar");
